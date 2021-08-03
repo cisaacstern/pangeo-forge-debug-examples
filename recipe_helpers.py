ed70e54cd5ac8103017714c796241196815b55ba
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-08-03 10:42:42 -0700

    update recipe

--- recipe_helpers.py
+++ recipe_helpers.py
@@ -18,13 +18,13 @@ def setup_targets(rec, name):
     fs_gcs = gcsfs.GCSFileSystem(anon=True,)
     fs_local = LocalFileSystem()
 
-    cache_base = 'gs://pangeo-forge-us-central1/pangeo-forge-cache/'
+    cache_base = 'gs://pangeo-forge-us-central1/pangeo-forge-cache'
 
     rec.input_cache = CacheFSSpecTarget(fs_gcs, f"{cache_base}/{name}")
     # rec.metadata_cache = MetadataTarget(fs_local, f"metadata/{name}")
     rec.target = MetadataTarget(fs_local, f"zarr_build/{name}.zarr")
 
-    print(f"""Targets for recipe_dict["{name}"] set to:
+    print(f"""Targets for recipe "{name}" set to:
     input_cache: {rec.input_cache.root_path}
     metadata_cache: N/A for recipes with `nitems_per_file=1`
     target: {rec.target.root_path}
